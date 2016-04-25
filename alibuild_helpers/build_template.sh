46fbc92ec5b0ba3b2e7377457c6e97b7a47c59f7
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-25 16:19:55 +0200

    Add LIBENVNAME and SONAME variables
    On mac:
    
        LIBENVNAME=DYLD_LIBRARY_PATH
        SONAME=dylib
    
    On linux:
    
        LIBENVNAME=LD_LIBRARY_PATH
        SONAME=so

--- alibuild_helpers/build_template.sh
+++ alibuild_helpers/build_template.sh
@@ -33,6 +33,16 @@ INCREMENTAL_BUILD_HASH=${INCREMENTAL_BUILD_HASH:-0}
 mkdir -p "$WORK_DIR/BUILD" "$WORK_DIR/SOURCES" "$WORK_DIR/TARS" \
          "$WORK_DIR/SPECS" "$WORK_DIR/INSTALLROOT"
 export BUILDROOT="$WORK_DIR/BUILD/$PKGHASH"
+case $ARCHITECTURE in
+  osx*)
+    export LIBENVNAME=DYLD_LIBRARY_PATH
+    export SONAME=dylib
+  ;;
+  *)
+    export LIBENVNAME=LD_LIBRARY_PATH
+    export SONAME=so
+  ;;
+esac
 
 # In case the repository is local, it means we are in development mode, so we
 # install directly in $WORK_DIR/$PKGPATH so that we can do make install
