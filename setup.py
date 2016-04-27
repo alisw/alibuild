476c4ef874c059d26e3be0c4107f4973e0119fa9
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-04-27 15:25:44 +0200

    Depend on argparse
    Some python 2.6 setup do not have it by default so we explicitly require
    it.

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyyaml'],
+    install_requires=['pyyaml','argparse'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
