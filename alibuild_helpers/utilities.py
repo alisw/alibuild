783e19b4f3ddcfbe573d70fe60bd63fd26c1e3fa
Dario Berzano <dario.berzano@cern.ch>
2016-05-03 20:40:05 +0200

    Detect Debian 7 and 8, and Ubuntu 16.04

--- alibuild_helpers/utilities.py
+++ alibuild_helpers/utilities.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import subprocess
+
 def format(s, **kwds):
   return s % kwds
 
@@ -22,10 +24,22 @@ def doDetectArch(hasOsRelease, osReleaseLines, platformTuple, platformSystem, pl
   if distribution.lower() == "ubuntu":
     version = version.split(".")
     version = version[0] + version[1]
-  if distribution in ["redhat", "centos"]:
+  elif distribution.lower() == "debian":
+    # http://askubuntu.com/questions/445487/which-ubuntu-version-is-equivalent-to-debian-squeeze
+    debian_ubuntu = { "7": "1204",
+                      "8": "1404" }
+    if version in debian_ubuntu:
+      distribution = "ubuntu"
+      version = debian_ubuntu[version]
+  elif distribution in ["redhat", "centos"]:
     distribution = distribution.replace("centos","slc").replace("redhat","slc").lower()
 
   processor = platformProcessor
+  if not processor:
+    # Sometimes platform.processor returns an empty string
+    p = subprocess.Popen(["uname", "-m"], stdout=subprocess.PIPE)
+    processor = p.stdout.read().strip()
+
   return format("%(d)s%(v)s_%(c)s",
                 d=distribution.lower(),
                 v=version.split(".")[0],
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -38,17 +38,56 @@ SUPPORT_URL="http://help.ubuntu.com/"
 BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"
 """
 
+UBUNTU_1604_OS_RELEASE = """
+NAME="Ubuntu"
+VERSION="16.04 LTS (Xenial Xerus)"
+ID=ubuntu
+ID_LIKE=debian
+PRETTY_NAME="Ubuntu 16.04 LTS"
+VERSION_ID="16.04"
+HOME_URL="http://www.ubuntu.com/"
+SUPPORT_URL="http://help.ubuntu.com/"
+BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"
+UBUNTU_CODENAME=xenial
+"""
+
+DEBIAN_7_OS_RELEASE = """
+PRETTY_NAME="Debian GNU/Linux 7 (wheezy)"
+NAME="Debian GNU/Linux"
+VERSION_ID="7"
+VERSION="7 (wheezy)"
+ID=debian
+ANSI_COLOR="1;31"
+HOME_URL="http://www.debian.org/"
+SUPPORT_URL="http://www.debian.org/support/"
+BUG_REPORT_URL="http://bugs.debian.org/"
+"""
+
+DEBIAN_8_OS_RELEASE = """
+PRETTY_NAME="Debian GNU/Linux 8 (jessie)"
+NAME="Debian GNU/Linux"
+VERSION_ID="8"
+VERSION="8 (jessie)"
+ID=debian
+HOME_URL="http://www.debian.org/"
+SUPPORT_URL="http://www.debian.org/support"
+BUG_REPORT_URL="https://bugs.debian.org/"
+"""
+
 architecturePayloads = [
   ['osx_x86-64', False, [], ('','',''), 'Darwin', 'x86-64'],
   ['slc5_x86-64', False, [], ('redhat', '5.XX', 'Boron'), 'Linux', 'x86-64'],
   ['slc6_x86-64', False, [], ('centos', '6.X', 'Carbon'), 'Linux', 'x86-64'],
   ['slc7_x86-64', False, [], ('centos', '7.X', 'Ptor'), 'Linux', 'x86-64'],
+  ['ubuntu1604_x86-64', True, UBUNTU_1604_OS_RELEASE.split("\n"), ('Ubuntu', '16.04', 'xenial'), 'Linux', 'x86-64'],
   ['ubuntu1510_x86-64', False, [], ('Ubuntu', '15.10', 'wily'), 'Linux', 'x86-64'],
   ['ubuntu1510_x86-64', True, UBUNTU_1510_OS_RELEASE.split("\n"), ('Ubuntu', '15.10', 'wily'), 'Linux', 'x86-64'],
   ['ubuntu1510_x86-64', True, UBUNTU_1510_OS_RELEASE.split("\n"), ('', '', ''), 'Linux', 'x86-64'], # ANACONDA case
   ['ubuntu1404_x86-64', True, UBUNTU_1404_OS_RELEASE.split("\n"), ('Ubuntu', '14.04', 'trusty'), 'Linux', 'x86-64'],
   ['ubuntu1404_x86-64', True, UBUNTU_1404_OS_RELEASE.split("\n"), ('', '', ''), 'Linux', 'x86-64'],
   ['ubuntu1404_x86-64', True, LINUX_MINT_OS_RELEASE.split("\n"), ('LinuxMint', '17.3', 'rosa'), 'Linux', 'x86-64'], # LinuxMint
+  ['ubuntu1204_x86-64', True, DEBIAN_7_OS_RELEASE.split("\n"), ('Debian', '7', 'wheezy'), 'Linux', 'x86-64'],
+  ['ubuntu1404_x86-64', True, DEBIAN_8_OS_RELEASE.split("\n"), ('Debian', '8', 'jessie'), 'Linux', 'x86-64']
 ]
 
 class TestArchitectures(unittest.TestCase):
