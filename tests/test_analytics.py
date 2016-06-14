05322e53bc927db1c69b3baa619f5e83d34ffc80
Giulio Eulisse <giulio.eulisse@cern.ch>
2016-06-14 15:01:39 +0200

    Fix test

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -12,34 +12,28 @@ def notInvoked():
 
 class TestAnalytics(unittest.TestCase):
   def test_analytics(self):
-    self.assertEqual(True, decideAnalytics(hasDisableFile=False,
-                                           hasDisableEnviron=False,
-                                           hasUuid=True,
-                                           isTty=False,
-                                           questionCallback=notInvoked))
     self.assertEqual(False,  decideAnalytics(hasDisableFile=False,
-                                             hasDisableEnviron=False,
                                              hasUuid=False,
                                              isTty=False,
                                              questionCallback=notInvoked))
     self.assertEqual(False, decideAnalytics(hasDisableFile=False,
-                                            hasDisableEnviron=False,
                                             hasUuid=False,
                                             isTty=True,
                                             questionCallback=noAnalytics))
     self.assertEqual(True, decideAnalytics(hasDisableFile=False,
-                                           hasDisableEnviron=False,
                                            hasUuid=False,
+                                           isTty=True,
+                                           questionCallback=yesAnalytics))
+    self.assertEqual(True, decideAnalytics(hasDisableFile=False,
+                                           hasUuid=True,
+                                           isTty=False,
+                                           questionCallback=notInvoked))
+    self.assertEqual(True, decideAnalytics(hasDisableFile=False,
+                                           hasUuid=True,
                                            isTty=True,
                                            questionCallback=yesAnalytics))
     self.assertEqual(False, decideAnalytics(hasDisableFile=True,
-                                           hasDisableEnviron=False,
-                                           hasUuid=False,
-                                           isTty=True,
-                                           questionCallback=yesAnalytics))
-    self.assertEqual(False, decideAnalytics(hasDisableFile=False,
-                                            hasDisableEnviron=True,
-                                            hasUuid=True,
+                                            hasUuid=False,
                                             isTty=True,
                                             questionCallback=yesAnalytics))
 True
