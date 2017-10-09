6dc32eefe15c85de4a9155a09c626fa4f16ab979
Giulio Eulisse <giulio.eulisse@cern.ch>
2017-10-09 15:43:09 +0200

    Raise an error if a dependency has a different capitalization

--- alibuild_helpers/utilities.py
+++ alibuild_helpers/utilities.py
@@ -293,6 +293,8 @@ def getPackageList(packages, specs, configDir, preferSystem, noSystem,
     filename = taps.get(lowerPkg, "%s/%s.sh" % (configDir, lowerPkg))
     err, spec, recipe = parseRecipe(getRecipeReader(filename, configDir))
     dieOnError(err, err)
+    dieOnError(spec["package"] != p,
+               "%s should be spelt %s." % (p, spec["package"]))
     dieOnError(spec["package"].lower() != lowerPkg,
                "%s.sh has different package field: %s" % (p, spec["package"]))
 
