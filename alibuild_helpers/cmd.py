41b004b174e4064aa5f3e1ae8a595f0932a90064
Dario Berzano <dario.berzano@cern.ch>
2017-11-16 01:23:01 +0100

    Force use of /bin/bash when checking system deps
    Also move Bash detection to cmd module and cache result

--- alibuild_helpers/cmd.py
+++ alibuild_helpers/cmd.py
@@ -1,6 +1,16 @@
+try:
+  from commands import getstatusoutput
+except ImportError:
+  from subprocess import getstatusoutput
 from alibuild_helpers.log import debug
-from alibuild_helpers.utilities import is_string
+from alibuild_helpers.utilities import is_string, cache
 import subprocess
+
+@cache
+def bashInterpreter():
+  # Given we started by enforcing /bin/bash we keep enforcing it, however if it's
+  # somewhere else on system, e.g. on NixOS, we fallback to the one in path.
+  return "bash" if getstatusoutput("/bin/bash --version")[0] else "/bin/bash"
 
 def execute(command, printer=debug):
   popen = subprocess.Popen(command, shell=is_string(command), stdout=subprocess.PIPE)
@@ -13,3 +23,14 @@ def execute(command, printer=debug):
   exitCode = popen.returncode
   return exitCode
 
+def getStatusOutputBash(command):
+  if is_string(command):
+    command = [ bashInterpreter(), "-c", command ]
+  popen = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  lines_iterator = iter(popen.stdout.readline, "")
+  txt = ""
+  for line in lines_iterator:
+    if not line: break
+    txt += line.decode('utf-8', "ignore") # yield line
+  txt += popen.communicate()[0].decode('utf-8', 'ignore')
+  return (popen.returncode, txt)
