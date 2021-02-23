d1ada880d7dcf34e2edc41aea372c07b221371a6
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2021-02-23 23:44:19 +0100

    Fix issue when encoding is latin1

--- alibuild_helpers/utilities.py
+++ alibuild_helpers/utilities.py
@@ -35,7 +35,10 @@ def is_string(s):
 def to_unicode(s):
   if sys.version_info[0] >= 3:
     if isinstance(s, bytes):
-      return s.decode("utf-8")  # to get newlines as such and not as escaped \n
+      try:
+        return s.decode("utf-8")  # to get newlines as such and not as escaped \n
+      except:
+        return s.decode("latin-1")  # Workaround issue with some special characters of latin-1 which are not understood by unicode
     return str(s)
   elif isinstance(s, str):
     return unicode(s, "utf-8")  # utf-8 is a safe assumption
