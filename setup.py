d21bd01e31ec5778ad3582767a6e9a9a884674e3
Timo Wilken <timo.wilken@cern.ch>
2021-08-13 11:05:27 +0200

    Import sys in setup.py
    To fix linter.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import sys
 import alibuild_helpers
 
 here = path.abspath(path.dirname(__file__))
