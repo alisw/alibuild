14261a2f0f07067e2fe257179d2b8d7a9d2a0ece
Timo Wilken <timo.wilken@cern.ch>
2021-11-03 22:04:31 +0100

    Clean up analytics and deps tests
    Use 4-space indentation and remove some intermediate variables.

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -2,40 +2,41 @@ import unittest
 from alibuild_helpers.analytics import decideAnalytics
 
 def noAnalytics():
-  return False
+    return False
 
 def yesAnalytics():
-  return True
+    return True
 
 def notInvoked():
-  assert(False)
+    assert(False)
 
 class TestAnalytics(unittest.TestCase):
-  def test_analytics(self):
-    self.assertEqual(False,  decideAnalytics(hasDisableFile=False,
-                                             hasUuid=False,
-                                             isTty=False,
-                                             questionCallback=notInvoked))
-    self.assertEqual(False, decideAnalytics(hasDisableFile=False,
-                                            hasUuid=False,
-                                            isTty=True,
-                                            questionCallback=noAnalytics))
-    self.assertEqual(True, decideAnalytics(hasDisableFile=False,
-                                           hasUuid=False,
-                                           isTty=True,
-                                           questionCallback=yesAnalytics))
-    self.assertEqual(True, decideAnalytics(hasDisableFile=False,
-                                           hasUuid=True,
-                                           isTty=False,
-                                           questionCallback=notInvoked))
-    self.assertEqual(True, decideAnalytics(hasDisableFile=False,
-                                           hasUuid=True,
-                                           isTty=True,
-                                           questionCallback=yesAnalytics))
-    self.assertEqual(False, decideAnalytics(hasDisableFile=True,
-                                            hasUuid=False,
-                                            isTty=True,
-                                            questionCallback=yesAnalytics))
-True
+    def test_analytics(self):
+        self.assertEqual(False,  decideAnalytics(hasDisableFile=False,
+                                                 hasUuid=False,
+                                                 isTty=False,
+                                                 questionCallback=notInvoked))
+        self.assertEqual(False, decideAnalytics(hasDisableFile=False,
+                                                hasUuid=False,
+                                                isTty=True,
+                                                questionCallback=noAnalytics))
+        self.assertEqual(True, decideAnalytics(hasDisableFile=False,
+                                               hasUuid=False,
+                                               isTty=True,
+                                               questionCallback=yesAnalytics))
+        self.assertEqual(True, decideAnalytics(hasDisableFile=False,
+                                               hasUuid=True,
+                                               isTty=False,
+                                               questionCallback=notInvoked))
+        self.assertEqual(True, decideAnalytics(hasDisableFile=False,
+                                               hasUuid=True,
+                                               isTty=True,
+                                               questionCallback=yesAnalytics))
+        self.assertEqual(False, decideAnalytics(hasDisableFile=True,
+                                                hasUuid=False,
+                                                isTty=True,
+                                                questionCallback=yesAnalytics))
+
+
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,83 +1,82 @@
-from __future__ import print_function
 try:
-  from unittest.mock import patch, call, MagicMock  # In Python 3, mock is built-in
-  from io import StringIO
+    from unittest.mock import patch, call, MagicMock  # In Python 3, mock is built-in
+    from io import StringIO
 except ImportError:
-  from mock import patch, call, MagicMock  # Python 2
-  from StringIO import StringIO
+    from mock import patch, call, MagicMock  # Python 2
+    from StringIO import StringIO
 try:
-  from collections import OrderedDict
+    from collections import OrderedDict
 except ImportError:
-  from ordereddict import OrderedDict
+    from ordereddict import OrderedDict
 
 from alibuild_helpers.deps import doDeps
 from argparse import Namespace
 import unittest
 
-RECIPE_DEFAULTS_RELEASE = """package: defaults-release
+RECIPES = {
+    "/dist/defaults-release.sh": """\
+package: defaults-release
 version: v1
 ---
-"""
-
-RECIPE_ALIROOT = """package: AliRoot
+""",
+    "/dist/gcc-toolchain.sh": """\
+package: GCC-Toolchain
+version: v1
+---
+""",
+    "/dist/aliroot.sh": """\
+package: AliRoot
 version: v1
 requires:
   - ROOT
   - GCC-Toolchain
 ---
-"""
-
-RECIPE_ROOT = """package: ROOT
+""",
+    "/dist/root.sh": """\
+package: ROOT
 version: v1
 build_requires:
   - GCC-Toolchain
 ---
-"""
+""",
+}
 
-RECIPE_GCC_TOOLCHAIN = """package: GCC-Toolchain
-version: v1
----
-"""
 
 class DepsTestCase(unittest.TestCase):
 
-  @patch("alibuild_helpers.deps.open")
-  @patch("alibuild_helpers.deps.execute")
-  @patch("alibuild_helpers.utilities.open")
-  def test_deps(self, mockOpen, mockExecute, mockDepsOpen):
-    mockOpen.side_effect = lambda f: { "/dist/aliroot.sh"          : StringIO(RECIPE_ALIROOT),
-                                       "/dist/root.sh"             : StringIO(RECIPE_ROOT),
-                                       "/dist/gcc-toolchain.sh"    : StringIO(RECIPE_GCC_TOOLCHAIN),
-                                       "/dist/defaults-release.sh" : StringIO(RECIPE_DEFAULTS_RELEASE) }[f]
+    @patch("alibuild_helpers.deps.open")
+    @patch("alibuild_helpers.deps.execute", new=lambda cmd: True)
+    @patch("alibuild_helpers.utilities.open", new=lambda f: StringIO(RECIPES[f]))
+    def test_deps(self, mockDepsOpen):
+        """Check doDeps doesn't raise an exception."""
+        dot = StringIO()
+        dot.name = ""
 
-    class NamedStringIO(StringIO):
-      name = ""
-    def depsOpen(fn, mode):
-      dot.name = fn
-      return dot
-    dot = NamedStringIO()
-    mockExecute.side_effect = lambda cmd: True
-    mockDepsOpen.side_effect = depsOpen
+        def depsOpen(fn, mode):
+            dot.name = fn
+            return dot
+        mockDepsOpen.side_effect = depsOpen
 
-    args = Namespace(workDir="/work",
-                     configDir="/dist",
-                     debug=False,
-                     docker=False,
-                     preferSystem=[],
-                     noSystem=True,
-                     architecture="slc7_x86-64",
-                     disable=[],
-                     neat=True,
-                     outdot="/tmp/out.dot",
-                     outgraph="/tmp/outgraph.pdf",
-                     package="AliRoot",
-                     defaults="release")
+        args = Namespace(workDir="/work",
+                         configDir="/dist",
+                         debug=False,
+                         docker=False,
+                         preferSystem=[],
+                         noSystem=True,
+                         architecture="slc7_x86-64",
+                         disable=[],
+                         neat=True,
+                         outdot="/tmp/out.dot",
+                         outgraph="/tmp/outgraph.pdf",
+                         package="AliRoot",
+                         defaults="release")
 
-    doDeps(args, MagicMock())
+        doDeps(args, MagicMock())
 
-    # Same check without explicit intermediate dotfile
-    args.outdot = None
-    doDeps(args, MagicMock())
+        # Same check without explicit intermediate dotfile
+        args.outdot = None
+        doDeps(args, MagicMock())
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
