a65ede13ac629e6c5711cdbb6e8910b2d3b633ee
Timo Wilken <timo.wilken@cern.ch>
2022-07-11 16:32:07 +0200

    Explicitly take write permission before deleting
    Golang installs stuff without write permissions for the owning user sometimes.
    In this case, take write permissions explicitly, so we can delete the old build
    directory.

--- alibuild_helpers/build_template.sh
+++ alibuild_helpers/build_template.sh
@@ -72,6 +72,9 @@ fi
 rm -fr "$WORK_DIR/INSTALLROOT/$PKGHASH"
 # We remove the build directory only if we are not in incremental mode.
 if [[ "$INCREMENTAL_BUILD_HASH" == 0 ]]; then
+  # Golang installs stuff without write permissions for ourselves sometimes.
+  # This makes a plain `rm -rf` fail, so give ourselves write permission first.
+  chmod -R o+w "$BUILDROOT"
   rm -rf "$BUILDROOT"
 fi
 mkdir -p "$INSTALLROOT" "$BUILDROOT" "$BUILDDIR" "$WORK_DIR/INSTALLROOT/$PKGHASH/$PKGPATH"
