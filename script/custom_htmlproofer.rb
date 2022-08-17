3d7950dc50140526797b49e75a3a137c44b2e27f
Timo Wilken <timo.wilken@cern.ch>
2022-08-17 15:06:11 +0200

    Don't enforce HTTPS links in docs
    We link to modules.sourceforge.net, which is HTTP-only.

--- script/custom_htmlproofer.rb
+++ script/custom_htmlproofer.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 require 'html-proofer'
-HTMLProofer.check_directory("./_site",
-                            :parallel => { :in_processes => 3 },
-                            :typhoeus => { :timeout => 100, :ssl_verifyhost => 2 }).run
+HTMLProofer.check_directory(
+  "./_site",
+  # We link to http://modules.sourceforge.net/, which is HTTP-only.
+  :enforce_https => false,
+  :parallel => { :in_processes => 3 },
+  :typhoeus => { :timeout => 100, :ssl_verifyhost => 2 },
+).run
