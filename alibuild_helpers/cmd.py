70b9460bd21d0a821374c2856f645ec42d7b0dfc
Timo Wilken <timo.wilken@cern.ch>
2022-11-04 15:29:25 +0100

    Timeout when git takes too long
    We want to avoid waiting for hours in CI if we can't update a git repository.
    
    Add a `timeout=` argument to our `alibuild_helpers.cmd.*` functions and enforce
    it, at least where feasible, i.e. on Python 3.

--- alibuild_helpers/cmd.py
+++ alibuild_helpers/cmd.py
@@ -1,14 +1,15 @@
 import os
 import os.path
 import sys
-from subprocess import Popen, PIPE, STDOUT
+import time
+from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
 from textwrap import dedent
 try:
   from shlex import quote  # Python 3.3+
 except ImportError:
   from pipes import quote  # Python 2.7
 
-from alibuild_helpers.log import debug, dieOnError
+from alibuild_helpers.log import debug, warning, dieOnError
 
 # Keep the linter happy
 if sys.version_info[0] >= 3:
@@ -43,19 +44,39 @@ def decode_with_fallback(data):
   return data
 
 
-def getoutput(command):
+def getoutput(command, timeout=None):
   """Run command, check it succeeded, and return its stdout as a string."""
   proc = Popen(command, shell=is_string(command), stdout=PIPE, stderr=PIPE)
-  stdout, stderr = proc.communicate()
+  try:
+    stdout, stderr = proc.communicate(timeout=timeout)
+  except TimeoutExpired:
+    warning("Process %r timed out; terminated", command)
+    proc.terminate()
+    stdout, stderr = proc.communicate()
+  except TypeError:
+    # On Python 2, we don't have the timeout= parameter. However, "regular"
+    # users shouldn't be running under Python 2 any more, so just don't timeout
+    # there and let the admins handle it manually.
+    stdout, stderr = proc.communicate()
   dieOnError(proc.returncode, "Command %s failed with code %d: %s" %
              (command, proc.returncode, decode_with_fallback(stderr)))
   return decode_with_fallback(stdout)
 
 
-def getstatusoutput(command):
+def getstatusoutput(command, timeout=None):
   """Run command and return its return code and output (stdout and stderr)."""
   proc = Popen(command, shell=is_string(command), stdout=PIPE, stderr=STDOUT)
-  merged_output, _ = proc.communicate()
+  try:
+    merged_output, _ = proc.communicate(timeout=timeout)
+  except TimeoutExpired:
+    warning("Process %r timed out; terminated", command)
+    proc.terminate()
+    merged_output, _ = proc.communicate()
+  except TypeError:
+    # On Python 2, we don't have the timeout= parameter. However, "regular"
+    # users shouldn't be running under Python 2 any more, so just don't timeout
+    # there and let the admins handle it manually.
+    merged_output, _ = proc.communicate()
   merged_output = decode_with_fallback(merged_output)
   # Strip a single trailing newline, if one exists, to match the behaviour of
   # subprocess.getstatusoutput.
@@ -64,10 +85,14 @@ def getstatusoutput(command):
   return proc.returncode, merged_output
 
 
-def execute(command, printer=debug):
+def execute(command, printer=debug, timeout=None):
   popen = Popen(command, shell=is_string(command), stdout=PIPE, stderr=STDOUT)
+  start_time = time.time()
   for line in iter(popen.stdout.readline, b""):
     printer("%s", decode_with_fallback(line).strip("\n"))
+    if timeout is not None and time.time() > start_time + timeout:
+      popen.terminate()
+      break
   out = decode_with_fallback(popen.communicate()[0]).strip("\n")
   if out:
     printer("%s", out)
