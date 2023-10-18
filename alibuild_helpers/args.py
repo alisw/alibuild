107fdeaf43d60086bbc803ada5ad17dcb9b9b037
Barthelemy <barthelemy.von.haller@cern.ch>
2023-10-18 14:39:37 +0200

    switch to slc9

--- alibuild_helpers/args.py
+++ alibuild_helpers/args.py
@@ -341,7 +341,7 @@ On Linux, x86-64:
    RHEL6 / SLC6 compatible: slc6_x86-64
    RHEL7 / CC7 compatible: slc7_x86-64
    RHEL8 / CC8 compatible: slc8_x86-64
-   RHEL9 compatible: rhel9_x86-64
+   RHEL9 / ALMA9 compatible: slc9_x86-64
    Ubuntu 20.04 compatible: ubuntu2004_x86-64
    Ubuntu 22.04 compatible: ubuntu2204_x86-64
    Fedora 33 compatible: fedora33_x86-64
@@ -355,7 +355,7 @@ On Mac, x86-64:
    Big Sur: osx_arm64
 """
 
-S3_SUPPORTED_ARCHS = "slc7_x86-64", "slc8_x86-64", "ubuntu2004_x86-64", "ubuntu2204_x86-64", "rhel9_x86-64"
+S3_SUPPORTED_ARCHS = "slc7_x86-64", "slc8_x86-64", "ubuntu2004_x86-64", "ubuntu2204_x86-64", "slc9_x86-64"
 
 def finaliseArgs(args, parser):
 
