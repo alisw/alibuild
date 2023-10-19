4662b1e9e40e4a91b81d6b31aa7014bacb0dfbba
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2023-10-19 09:18:01 +0200

    Add support for sapling
    Sapling is a somewhat novel SCM from Facebook / Meta.
    
    It is fully compatible with Git and provides a more powerful
    yet userfriendly frontend, modeled after mercurial.
    
    This adds Sapling support in alibuild, including:
    
    * Support for alidist sapling checkouts
    * Support for development packages which were cloned via sapling

--- alibuild_helpers/build.py
+++ alibuild_helpers/build.py
@@ -14,6 +14,7 @@ from alibuild_helpers.utilities import Hasher
 from alibuild_helpers.utilities import yamlDump
 from alibuild_helpers.utilities import resolve_tag, resolve_version
 from alibuild_helpers.git import git, clone_speedup_options, Git
+from alibuild_helpers.sl import sapling, Sapling
 from alibuild_helpers.sync import (NoRemoteSync, HttpRemoteSync, S3RemoteSync,
                                    Boto3RemoteSync, RsyncRemoteSync)
 import yaml
@@ -62,6 +63,10 @@ def update_git_repos(args, specs, buildOrder, develPkgs):
 
     def update_repo(package, git_prompt):
         specs[package]["scm"] = Git()
+        if package in develPkgs:
+          localCheckout = os.path.join(os.getcwd(), specs[package]["package"])
+          if exists("%s/.sl" % localCheckout):
+            specs[package]["scm"] = Sapling()
         updateReferenceRepoSpec(args.referenceSources, package, specs[package],
                                 fetch=args.fetchRepos,
                                 usePartialClone=not args.docker,
@@ -364,10 +369,19 @@ def doBuild(args, parser):
   if not exists(specDir):
     makedirs(specDir)
 
-  # By default Git is our SCM, so we find the commit hash of alidist
-  # using it.
-  scm = Git()
-  os.environ["ALIBUILD_ALIDIST_HASH"] = scm.checkedOutCommitName(directory=args.configDir)
+  # If the alidist workdir contains a .sl directory, we use Saplign as SCM
+  # otherwise we default to git (without checking for the actual presence of 
+  # .git). 
+  # We do it this way, because we have one test which uses an embedded folder 
+  # in the alibuild source, which therefore does not contain a .git directory
+  # and falls back to the alibuild git commit.
+  scm = exists("%s/.sl" % args.configDir) and Sapling() or Git()
+  try:
+    checkedOutCommitName = scm.checkedOutCommitName(directory=args.configDir)
+  except:
+    error("Cannot find SCM directory in %s.", args.configDir)
+    return 1
+  os.environ["ALIBUILD_ALIDIST_HASH"] = checkedOutCommitName
 
   debug("Building for architecture %s", args.architecture)
   debug("Number of parallel builds: %d", args.jobs)
