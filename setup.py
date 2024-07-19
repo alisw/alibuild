9967470e8c03b0b157c8fad8756fdfe3771604ed
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-07-19 13:09:01 +0200

    Fix parenthesis priority issue

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
         # The 7.* series removed support for Python 3.6.
         'setuptools_scm<7.0.0' if sys.version_info < (3, 7) else
         'setuptools_scm'
-    ] + ['packaging<=23'] if sys.version_info <(3, 7) else [],
+    ] + (['packaging<=23'] if sys.version_info <(3, 7) else []),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
