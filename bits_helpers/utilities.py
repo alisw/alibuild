f257ca6916d0e8841b9ff7e19485f6011bd06233
Giulio Eulisse <10544+ktf@users.noreply.github.com>
2024-10-15 16:18:04 +0200

    Add assertions about spec / recipe validity. (#874)

--- bits_helpers/utilities.py
+++ bits_helpers/utilities.py
@@ -473,6 +473,10 @@ def getPackageList(packages, specs, configDir, preferSystem, noSystem,
 
     err, spec, recipe = parseRecipe(getRecipeReader(filename, configDir))
     dieOnError(err, err)
+    # Unless there was an error, both spec and recipe should be valid.
+    # otherwise the error should have been caught above.
+    assert(spec is not None)
+    assert(recipe is not None)
     dieOnError(spec["package"].lower() != pkg_filename,
                "%s.sh has different package field: %s" % (p, spec["package"]))
     spec["pkgdir"] = pkgdir
