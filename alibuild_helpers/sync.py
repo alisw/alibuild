f56a6fdbfdbcdbc839104fcdd33077fac94badc8
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-11-21 17:19:55 +0100

    Revert HTTP upload_symlinks behaviour change

--- alibuild_helpers/sync.py
+++ alibuild_helpers/sync.py
@@ -218,7 +218,7 @@ class HttpRemoteSync:
               os.path.join(self.workdir, links_path, linkname))
 
   def upload_symlinks_and_tarball(self, spec):
-    dieOnError(True, "HTTP backend does not support uploading directly")
+    pass
 
 
 class RsyncRemoteSync:
