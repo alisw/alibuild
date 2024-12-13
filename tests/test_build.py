0163b70b4e937997b545dedda3a691792e462d8f
Sergio <sergio.garcia.de.la.cruz@cern.ch>
2024-12-13 14:51:56 +0100

    Fix tests

--- tests/test_build.py
+++ tests/test_build.py
@@ -264,6 +264,7 @@ class BuildTestCase(unittest.TestCase):
         args = Namespace(
             remoteStore="",
             writeStore="",
+            no_remote_store=True,
             referenceSources="/sw/MIRROR",
             docker=False,
             dockerImage=None,
