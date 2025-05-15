3ff5e6fb13c41f5b792249c34093f5cf0811726d
Sergio Garcia <sergio.garcia.de.la.cruz@cern.ch>
2025-05-15 14:14:40 +0200

    test_deps: Fix open mock

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,5 +1,6 @@
 from unittest.mock import patch, MagicMock
 from io import StringIO
+import os.path
 
 from bits_helpers.deps import doDeps
 from argparse import Namespace
@@ -65,11 +66,16 @@ class DepsTestCase(unittest.TestCase):
                          package="AliRoot",
                          defaults="release")
 
-        doDeps(args, MagicMock())
 
-        # Same check without explicit intermediate dotfile
-        args.outdot = None
-        doDeps(args, MagicMock())
+        def fake_exists(n):
+            return True if n in RECIPES else False
+
+        with patch.object(os.path, "exists", fake_exists):
+          doDeps(args, MagicMock())
+
+          # Same check without explicit intermediate dotfile
+          args.outdot = None
+          doDeps(args, MagicMock())
 
 
 if __name__ == '__main__':
