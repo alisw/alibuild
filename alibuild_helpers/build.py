b05994e141ac4a77db7cbf76adc50485c9981ea1
Sergio Garcia <47090312+singiamtel@users.noreply.github.com>
2025-11-26 15:54:37 +0100

    Remove pkg_resources (#906)
    It's deprecated and removed since Python 3.12
    
    Co-authored-by: Giulio Eulisse <10544+ktf@users.noreply.github.com>

--- alibuild_helpers/build.py
+++ alibuild_helpers/build.py
@@ -1019,15 +1019,10 @@ def doBuild(args, parser):
 
     # The actual build script.
     debug("spec = %r", spec)
-
-    cmd_raw = ""
-    try:
-      fp = open(dirname(realpath(__file__))+'/build_template.sh', 'r')
-      cmd_raw = fp.read()
-      fp.close()
-    except Exception:
-      from pkg_resources import resource_string
-      cmd_raw = resource_string("alibuild_helpers", 'build_template.sh')
+    
+    fp = open(dirname(realpath(__file__))+'/build_template.sh', 'r')
+    cmd_raw = fp.read()
+    fp.close()
 
     if args.docker:
       cachedTarball = re.sub("^" + workDir, "/sw", spec["cachedTarball"])
